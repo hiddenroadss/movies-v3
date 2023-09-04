d7598f5fc0bc77719173334116e2c9f4a6dfc586
Taras Savchenko <hiddenroadss@gmail.com>
2023-09-04 03:04:36 +0300

    feat(backend): add validation pipe

--- projects/api/src/movie/dto/create-movie.dto.ts
+++ projects/api/src/movie/dto/create-movie.dto.ts
@@ -1,13 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { MinLength, IsString, IsNotEmpty, IsDate } from 'class-validator';
 
 export class CreateMovieDto {
   @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
   title: string;
 
   @ApiProperty({ required: false })
   director?: string;
 
   @ApiProperty({ required: false })
+  @IsDate()
   releaseDate?: Date;
 
   @ApiProperty({ required: false })
