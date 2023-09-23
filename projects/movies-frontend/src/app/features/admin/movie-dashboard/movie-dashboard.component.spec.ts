182d2548f45e84e261be3d907bf55e64d6c831b9
Taras Savchenko <hiddenroadss@gmail.com>
2023-09-23 18:24:56 +0300

    test(movie-dashboard): add tests for movies dashboard

--- projects/movies-frontend/src/app/features/admin/movie-dashboard/movie-dashboard.component.spec.ts
+++ projects/movies-frontend/src/app/features/admin/movie-dashboard/movie-dashboard.component.spec.ts
@@ -2,22 +2,148 @@ import { createComponentFactory, Spectator } from '@ngneat/spectator';
 import { MovieDashboardComponent } from './movie-dashboard.component';
 import { MoviesService } from '@core/services/api/movies.service';
 import { of } from 'rxjs';
+import { moviesStub } from 'src/app/testing/stubs/movies';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatTableHarness } from '@angular/material/table/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatPaginatorHarness } from '@angular/material/paginator/testing';
+import { MatCheckboxHarness } from '@angular/material/checkbox/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
 
 describe('MovieDashboardComponent', () => {
   let spectator: Spectator<MovieDashboardComponent>;
+  let moviesService: MoviesService;
+  let loader: HarnessLoader;
 
   const createComponent = createComponentFactory({
     component: MovieDashboardComponent,
     providers: [
-      { provide: MoviesService, useValue: { getMovies: () => of([]) } },
+      {
+        provide: MoviesService,
+        useValue: {
+          getMovies: () => of([]),
+          updateMovie: () => of({}),
+          deleteMovie: () => of({}),
+        },
+      },
     ],
   });
 
   beforeEach(() => {
     spectator = createComponent();
+    moviesService = spectator.inject(MoviesService);
+    loader = TestbedHarnessEnvironment.loader(spectator.fixture);
   });
 
-  it('should create', () => {
-    expect(spectator.component).toBeTruthy();
+  it('should initialize the table and paginator with movie data', async () => {
+    spyOn(moviesService, 'getMovies').and.returnValue(of(moviesStub));
+    spectator.component.reloadMovies$.next();
+    const table = await loader.getHarness(MatTableHarness);
+    const paginator = await loader.getHarness(MatPaginatorHarness);
+
+    expect(table).toBeTruthy();
+    const cellText = await table.getCellTextByColumnName();
+
+    expect(cellText['title'].text).toEqual(
+      moviesStub.map(movie => movie.title)
+    );
+
+    expect(paginator).toBeTruthy();
   });
+
+  it('should open the dialog when the edit button is clicked and update movie data', async () => {
+    spyOn(moviesService, 'getMovies').and.returnValue(of(moviesStub));
+    spectator.component.reloadMovies$.next();
+    const editButtons = await loader.getAllHarnesses(
+      MatButtonHarness.with({ selector: '[data-test="edit-button"]' })
+    );
+    spyOn(spectator.component, 'editMovie').and.stub();
+
+    await editButtons[0].click();
+
+    expect(spectator.component.editMovie).toHaveBeenCalledOnceWith(
+      moviesStub[0]
+    );
+  });
+
+  it('should select a row when the checkbox is clicked', async () => {
+    spyOn(moviesService, 'getMovies').and.returnValue(of(moviesStub));
+    spectator.component.reloadMovies$.next();
+    const checkboxes = await loader.getAllHarnesses(
+      MatCheckboxHarness.with({ selector: '[data-test="select-checkbox"]' })
+    );
+    spyOn(spectator.component.selection, 'toggle').and.callThrough();
+    await checkboxes[1].check();
+    expect(spectator.component.selection.toggle).toHaveBeenCalledWith(
+      moviesStub[1]
+    );
+  });
+
+  it('should delete a single movie when the delete button is clicked', async () => {
+    spyOn(moviesService, 'getMovies').and.returnValue(of(moviesStub));
+    spectator.component.reloadMovies$.next();
+    const checkboxes = await loader.getAllHarnesses(
+      MatCheckboxHarness.with({ selector: '[data-test="select-checkbox"]' })
+    );
+    await checkboxes[2].check();
+    const deleteButtons = await loader.getHarness(
+      MatButtonHarness.with({ selector: '[data-test="delete-button"]' })
+    );
+    spyOn(spectator.component, 'deleteMovie').and.callThrough();
+    await deleteButtons?.click();
+    expect(spectator.component.deleteMovie).toHaveBeenCalled();
+  });
+
+  it('should check all rows when the select all checkbox is clicked', async () => {
+    spyOn(moviesService, 'getMovies').and.returnValue(of(moviesStub));
+    spectator.component.reloadMovies$.next();
+    const selectAllCheckbox = await loader.getHarness(
+      MatCheckboxHarness.with({ selector: '[data-test="select-all-checkbox"]' })
+    );
+    await selectAllCheckbox.check();
+    expect(spectator.component.isAllSelected()).toBeTrue();
+    const selectionLength = spectator.component.selection.selected.length;
+    expect(selectionLength).toEqual(moviesStub.length);
+  });
+
+  it('should select and deselect all rows with toggleAllRows', () => {
+    spyOn(moviesService, 'getMovies').and.returnValue(of(moviesStub));
+    spectator.component.reloadMovies$.next();
+
+    // Select all rows
+    spectator.component.toggleAllRows();
+    expect(spectator.component.isAllSelected()).toBeTrue();
+    expect(spectator.component.selection.selected.length).toEqual(
+      moviesStub.length
+    );
+
+    // Deselect all rows
+    spectator.component.toggleAllRows();
+    expect(spectator.component.isAllSelected()).toBeFalse();
+    expect(spectator.component.selection.selected.length).toEqual(0);
+  });
+
+  // it('should properly update dataSource after deleting a single movie', async () => {
+  //   spyOn(moviesService, 'getMovies').and.returnValue(of(moviesStub));
+  //   spyOn(moviesService, 'deleteMovie').and.returnValue(of());
+  //   spectator.component.reloadMovies$.next();
+
+  //   const initialDataSourceLength = spectator.component.dataSource.data.length;
+
+  //   const checkboxes = await loader.getAllHarnesses(
+  //     MatCheckboxHarness.with({ selector: '[data-test="select-checkbox"]' })
+  //   );
+  //   await checkboxes[2].check();
+
+  //   const deleteButton = await loader.getHarness(
+  //     MatButtonHarness.with({ selector: '[data-test="delete-button"]' })
+  //   );
+  //   spyOn(spectator.component, 'deleteMovie').and.callThrough();
+  //   await deleteButton.click();
+
+  //   expect(spectator.component.deleteMovie).toHaveBeenCalled();
+  //   expect(spectator.component.dataSource.data.length).toEqual(
+  //     initialDataSourceLength - 1
+  //   );
+  // });
 });
--- projects/movies-frontend/src/app/testing/stubs/movies.ts
+++ projects/movies-frontend/src/app/testing/stubs/movies.ts
@@ -9,3 +9,58 @@ export const movieStub: Movie = {
   description: 'test',
   poster: 'test',
 };
+
+export const moviesStub: Movie[] = [
+  {
+    id: 1,
+    title: 'The Shawshank Redemption',
+    director: 'Frank Darabont',
+    releaseDate: new Date('1994-10-14'),
+    tags: [
+      { id: 1, name: 'Drama' },
+      { id: 2, name: 'Crime' },
+    ],
+    description:
+      'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+    poster: 'https://www.imdb.com/title/tt0111161/mediaviewer/rm3582021376',
+  },
+  {
+    id: 2,
+    title: 'The Godfather',
+    director: 'Francis Ford Coppola',
+    releaseDate: new Date('1972-03-24'),
+    tags: [
+      { id: 1, name: 'Drama' },
+      { id: 2, name: 'Crime' },
+    ],
+    description:
+      'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+    poster: 'https://www.imdb.com/title/tt0068646/mediaviewer/rm3582021376',
+  },
+  {
+    id: 3,
+    title: 'The Dark Knight',
+    director: 'Christopher Nolan',
+    releaseDate: new Date('2008-07-18'),
+    tags: [
+      { id: 1, name: 'Drama' },
+      { id: 2, name: 'Action' },
+    ],
+    description:
+      'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+    poster: 'https://www.imdb.com/title/tt0468569/mediaviewer/rm3582021376',
+  },
+  {
+    id: 4,
+    title: 'The Lord of the Rings: The Fellowship of the Ring',
+    director: 'Peter Jackson',
+    releaseDate: new Date('2001-12-19'),
+    tags: [
+      { id: 1, name: 'Fantasy' },
+      { id: 2, name: 'Adventure' },
+    ],
+    description:
+      'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
+    poster: 'https://www.imdb.com/title/tt0120737/mediaviewer/rm3582021376',
+  },
+];
