fa2699d64944a9f89a1ba113369552e40002bf0e
Taras Savchenko <hiddenroadss@gmail.com>
2023-11-02 17:38:28 +0200

    refactor(styles): add tailwind & remove some mat components

--- projects/movies-frontend/src/app/core/components/header/header.component.ts
+++ projects/movies-frontend/src/app/core/components/header/header.component.ts
@@ -1,14 +1,14 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  @Output() toggleSidenav = new EventEmitter<void>();
 }
--- projects/movies-frontend/src/app/core/core.module.ts
+++ projects/movies-frontend/src/app/core/core.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 @NgModule({
   declarations: [],
-  imports: [HeaderComponent, SideMenuComponent],
-  exports: [HeaderComponent, SideMenuComponent]
+  imports: [HeaderComponent],
+  exports: [HeaderComponent]
 })
 export class CoreModule {}
